390f68a843cf93bc14984a134b10c43ef1a2504b
Festus Muia <fstsmuia@gmail.com>
2025-06-25 17:42:11 +0300

    refactor: blogs router

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
 
 const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
 
-const Blog = require('./models/blog')
-
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 const PORT = config.PORT
 
