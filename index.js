4a6c3fa40b142af62486ce071a429f3c6a0b3792
Festus Muia <fstsmuia@gmail.com>
2025-06-25 17:55:41 +0300

    refactor: seperated app from server

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const blogsRouter = require('./controllers/blogs')
 
-const express = require('express')
-const mongoose = require('mongoose')
-
-const app = express()
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use('/api/blogs', blogsRouter)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-const PORT = config.PORT
-
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
