3ce588c2fb2711f55d3cc0131bfc3cc6bfae11b7
Festus Muia <fstsmuia@gmail.com>
2025-06-26 16:46:53 +0300

    add: favoriteBlogs and mostBlogs helper and test functions

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -8,7 +10,34 @@ const totalLikes = (blogs) => {
     }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    return blogs.reduce((maxLikesBlog, blog) => {
+        return blog.likes > maxLikesBlog.likes? blog : maxLikesBlog
+    })
+}
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    const authors = lodash.countBy(blogs, 'author')
+    const authorMostBlogs = Object.keys(authors).
+            reduce((maxAuthor, currAuthor) => 
+                authors[currAuthor] > authors[maxAuthor]? currAuthor : maxAuthor)
+    return {
+        author: authorMostBlogs,
+        blogs: authors[authorMostBlogs]
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
