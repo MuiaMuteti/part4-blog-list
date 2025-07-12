5406aae92dffef9eeb30c94ce8723c39f9a6aab6
Festus Muia <fstsmuia@gmail.com>
2025-07-12 16:09:52 +0300

    refactor: group unit tests per group

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { beforeEach, test, after } = require('node:test')
+const { beforeEach, test, after, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const app = require('../app')
@@ -8,68 +8,87 @@ const supertest = require('supertest')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(helper.initialBlogs)
-})
-
-test('all notes are returned and in json format', async () => {
-    const response = await api
-                    .get('/api/blogs')
-                    .expect(200)
-                    .expect('Content-Type', /application\/json/)
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
-})
-
-test('blog has id property', async () => {
-    const response = await api.get('/api/blogs')
-    response.body.forEach(blog => {
-        assert(blog.hasOwnProperty('id'))
-        assert(!blog.hasOwnProperty('_id'))
+describe('when there is initially some blogs saved', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        await Blog.insertMany(helper.initialBlogs)
     })
-})
 
-test('a blog is added', async () => {
-    const newBlog = {
-        title: "Net Runners",
-        author: "Arasaka",
-        url: "cyberpunk.com",
-        likes: 100000
-    }
+    test('all blogs are returned and in json format', async () => {
+        const response = await api
+                        .get('/api/blogs')
+                        .expect(200)
+                        .expect('Content-Type', /application\/json/)
+        assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
 
-    await api.post('/api/blogs')
-            .send(newBlog)
-            .expect(201)
-            .expect('Content-Type', /application\/json/)
+    test('all blogs returned have unique identifier property', async () => {
+        const response = await api.get('/api/blogs')
+        response.body.forEach(blog => {
+            assert(blog.hasOwnProperty('id'))
+            assert(!blog.hasOwnProperty('_id'))
+        })
+    })
 
-    const blogsDB = await helper.blogsInDB()
-    assert.strictEqual(blogsDB.length, helper.initialBlogs.length + 1)
+    describe('addition of a new blog', () => {
+        test('succeeds with valid data', async () => {
+            const newBlog = {
+                title: "Net Runners",
+                author: "Arasaka",
+                url: "cyberpunk.com",
+                likes: 100000
+            }
 
-    const titles = blogsDB.map(blog => blog.title)
-    assert(titles.includes(newBlog.title))
-})
+            await api.post('/api/blogs')
+                    .send(newBlog)
+                    .expect(201)
+                    .expect('Content-Type', /application\/json/)
 
-test('missing likes property defaults to zero', async () => {
-    const newBlog = {
-        title: "Net Runners",
-        author: "Arasaka",
-        url: "cyberpunk.com"
-    }
-    const response = await api.post('/api/blogs').send(newBlog)
-    assert.strictEqual(response.body.likes, 0)
-})
+            const blogsDB = await helper.blogsInDB()
+            assert.strictEqual(blogsDB.length, helper.initialBlogs.length + 1)
 
-test('missing title or url results to status code 400', async () => {
-    const newBlog = {
-        author: "Arasaka",
-        likes: 100000
-    }
-    await api.post('/api/blogs')
-            .send(newBlog)
-            .expect(400)
+            const titles = blogsDB.map(blog => blog.title)
+            assert(titles.includes(newBlog.title))
+        })
 
-    const blogsDB = await helper.blogsInDB()
-    assert.strictEqual(blogsDB.length, helper.initialBlogs.length)
+        test('missing the likes property succeeds and the property defaults to zero', async () => {
+            const newBlog = {
+                title: "Net Runners",
+                author: "Arasaka",
+                url: "cyberpunk.com"
+            }
+            const response = await api.post('/api/blogs').send(newBlog).expect(201)
+            assert.strictEqual(response.body.likes, 0)
+        })
+
+        test('fails with status code 400 if title is missing', async () => {
+            const newBlog = {
+                author: "Arasaka",
+                likes: 100000,
+                url: "cyberpunk.com"
+            }
+            await api.post('/api/blogs')
+                    .send(newBlog)
+                    .expect(400)
+
+            const blogsDB = await helper.blogsInDB()
+            assert.strictEqual(blogsDB.length, helper.initialBlogs.length)
+        })
+
+        test('fails with status code 400 if url is missing', async () => {
+            const newBlog = {
+                title: "Net Runners",
+                author: "Arasaka",
+                likes: 100000
+            }
+            await api.post('/api/blogs')
+                    .send(newBlog)
+                    .expect(400)
+
+            const blogsDB = await helper.blogsInDB()
+            assert.strictEqual(blogsDB.length, helper.initialBlogs.length)
+        })
+    })
 })
 
 after(async () => {
