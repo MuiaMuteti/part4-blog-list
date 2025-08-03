c463952391694881fb8d3169c5c5e23d323734b0
Festus Muia <fstsmuia@gmail.com>
2025-08-03 11:07:10 +0300

    add: routes for creating and reading users

---
+++ controllers/users.js
@@ -0,0 +1,33 @@
+const usersRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body
+
+    if (!password || password.length < 3) {
+        return response
+            .status(400)
+            .json({ error: 'Password required and must be at least 3 characters long' })
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const newUser = new User({
+        username,
+        name,
+        passwordHash
+    })
+
+    const savedUser = await newUser.save()
+    
+    response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
