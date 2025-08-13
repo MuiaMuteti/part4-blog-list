e03d6fb8774479a265bf570c58749d7b76297e7b
Festus Muia <fstsmuia@gmail.com>
2025-08-13 19:11:17 +0300

    add: new blogs are only created by logged in users

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -10,10 +19,16 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response, next) => {
   const { title, author, url, likes } = request.body
 
-  const user = await User.findOne()
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
 
-  if (!user || !user._id) {
-    return response.status(400).json({ error: 'user is missing or invalid' })
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  
+  const user = await User.findById(decodedToken.id)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId is missing or invalid' })
   }
 
   const blog = new Blog({
