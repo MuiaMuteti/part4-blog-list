fb184f16f5830b3c5fdb71465eed45a589f53aa9
Festus Muia <fstsmuia@gmail.com>
2025-08-14 17:58:08 +0300

    refactor: token extractor middleware

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ mongoose.connect(config.MONGODB_URI).then(() => {
 
 // app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
